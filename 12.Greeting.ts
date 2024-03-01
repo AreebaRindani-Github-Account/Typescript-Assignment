730fff5d4108a652dc169e71a27310ed62f8066a
AreebaRindani-Github-Account <161863410+AreebaRindani-Github-Account@users.noreply.github.com>
2024-03-02 00:59:07 +0500

    Add files via upload

---
+++ 12.Greeting.ts
@@ -0,0 +1,9 @@
+let names :string[] = ['Muskan','Mehak','Laiba','Zahra'] ;
+
+const message :string = 'Welcome to typescript Class: '
+
+for(let i=0; i<names.length; i++){
+
+    console.log(message + names[i]);
+    
+}
