3a2d4f8bd8ab52feeefa7ca172abc9093ef656f0
AreebaRindani-Github-Account <161863410+AreebaRindani-Github-Account@users.noreply.github.com>
2024-03-02 03:22:55 +0500

    Add files via upload

---
+++ 20.Empty_Array.ts
@@ -0,0 +1,11 @@
+// Think of something you could store in a array.
+let items :string[] = ['Pakistan','Karachi','Urdu',];
+
+// Write a program that creates a list containing there items.
+let items2 :string[] = [] ;
+
+items2.push('Germany');
+items2.push('England');
+items2.push('India');
+
+console.log(items2);
