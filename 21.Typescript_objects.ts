1a1aa37622118d6bb19b8f616a9a8601e2b86d8e
AreebaRindani-Github-Account <161863410+AreebaRindani-Github-Account@users.noreply.github.com>
2024-03-02 03:24:27 +0500

    Add files via upload

---
+++ 21.Typescript_objects.ts
@@ -0,0 +1,19 @@
+
+// Write a program that creates objects containing these items.
+ 
+// Datatype of car object. 
+interface car {
+    maker : string
+    color : string
+    automatic : boolean
+}
+
+// car object
+let car = {
+    maker : 'toyota',
+    color : 'Black',
+    automatic : true
+}
+
+// print car
+console.log(car);
