d87d3eab01fd13d9ee218e4661151731eba95a84
AreebaRindani-Github-Account <161863410+AreebaRindani-Github-Account@users.noreply.github.com>
2024-03-21 07:02:39 +0500

    Add files via upload

---
+++ 35.animals.ts
@@ -0,0 +1,12 @@
+//  Animals: think of at least three different animals that have a common characteristic. 
+// store the names of these animals in a list, and then use a for loop to print out the name of each animal.
+// Modify your program to print a statement about each animal, such as a dog would make a great pet. add a 
+// line at the end of your program stating what these animal have in common. you could print a sentence 
+// such as any of these animals would make a great pet!
+
+let Animals: string[] = [ 'dog','cat','rabbit'];
+
+for(let Animal of Animals){
+    console.log(`A ${Animal} would make a great pet.`);
+}
+console.log(`Any of these animals would make a great pet!`);
